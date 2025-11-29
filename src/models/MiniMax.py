11892853f64dbceb018d6bf1f991d535f71aad64
Jonathan Soto Ortega <35411404+jsoto1614@users.noreply.github.com>
2025-11-29 10:51:58 -0500

    Added comments to the script
    Commented functions and classes so reader has a better idea of what the code is doing.

--- src/models/MiniMax.py
+++ src/models/MiniMax.py
@@ -9,9 +9,12 @@ import copy
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
+# global time step 
 DT = 0.1
 
+# Player class controls player movement according to physics
 class Player():
+    # constructor for class
     def __init__(self, id, p90a, x0, y0, s, a, dir, side, role, pos, predict=True):
         self.id = id
         self.max_a = p90a
@@ -25,6 +28,7 @@ class Player():
         self.coordinates = [self.coordinate.copy()]
         self.predict = predict
 
+    # moves player in a certain direction by a number of timesteps
     def move(self, minimax_direction, timesteps=1):
         new_a = np.array([self.max_a * sin(radians(minimax_direction)), self.max_a * cos(radians(minimax_direction))])
         for t in range(timesteps):
@@ -36,7 +40,8 @@ class Player():
             self.coordinate += self.velocity * DT
             self.coordinates.append(self.coordinate.copy())       
         return self.coordinates
-    
+
+    # get the direction in which the player can move
     def legalActions(self, right_dir):
         if not self.predict:
             return [self.direction]
@@ -45,8 +50,9 @@ class Player():
         else:
             return [270, 345, 195]
     
-
+# Play class generates the game tree according to miniMax alpha-beta pruning implementation
 class Play():
+    # constructor
     def __init__(self, game_id, play_id, players, num_frames, ball_x, ball_y, right_dir, num_moves=0):
         self.game_id = game_id
         self.play_id = play_id
@@ -62,7 +68,7 @@ class Play():
         self.right_dir = right_dir
         self.num_moves = num_moves
     
-    
+    # collects trajectory predictions for players where it is required
     def generatePredictions(self):
         pred = pd.DataFrame(columns=['game_id', 'play_id', 'nfl_id', 'frame_id', 'x', 'y'])
         for player in self.offense + self.defense:
@@ -78,7 +84,7 @@ class Play():
                 pred = pd.concat([pred, new_df], ignore_index=True)
         return pred
 
-    
+    # generates the successor state for a play
     def generateSuccessor(self, playerIndex, action, num_moves, offense=True):
         nextPlay = Play(self.game_id, self.play_id, copy.deepcopy(self.offense + self.defense), self.num_frames, self.ball_position[0], self.ball_position[1], self.right_dir, num_moves)
         t = floor(self.num_frames/2)
@@ -90,21 +96,23 @@ class Play():
         else:
             nextPlay.defense[playerIndex].move(action, t)
         return nextPlay
-    
+
+    # evaluation function that gives higher values for a receiver being close to the ball landing position, and lower values when a defender is close to an offensive player.
     def evaluationFunction(self):
         total_score = 0
-        for player in (self.offense + self.offense):
+        for player in (self.offense):
             weight = 1
             if player.receiver:
                 weight = 50
             dist_to_ball = ((player.coordinate[0] - self.ball_position[0])**2 + (player.coordinate[1]- self.ball_position[1])**2)**0.5
             total_score += -weight*dist_to_ball
-            for defender in (self.defense+self.defense):
+            for defender in (self.defense):
                 dist_to_defender = ((player.coordinate[0] - defender.coordinate[0])**2 + (player.coordinate[1] - defender.coordinate[1])**2)**0.5
                 if dist_to_defender < 2.0:
                     total_score += -100
         return total_score
-    
+
+    # recursive component of miniMax algorithm that takes care of the defensive players
     def minIter(self, defenseIndex, alpha, beta):
         min_value = float('inf')
         min_pred = None
@@ -124,7 +132,8 @@ class Play():
                 break
             beta = min(beta, min_value)
         return min_value, min_pred
-    
+
+    # recursive component of miniMax algorithm that takes care of the offensive players
     def maxIter(self, offenseIndex, alpha, beta):      
         max_value = float('-inf')
         max_pred = None
@@ -145,14 +154,15 @@ class Play():
                 break
             alpha = max(alpha, max_value)
         return max_value, max_pred
-    
+
+    # starts the miniMax algorithm
     def miniMax(self):
         alpha = float('-inf')
         beta = float('inf')
         max, predictions = self.maxIter(0, alpha, beta)
         return predictions
     
-
+# function that creates all required objects to generate the required predictions for a play
 def predict(test: pl.DataFrame, test_input: pl.DataFrame) -> pl.DataFrame | pd.DataFrame:
 
     # Compute 90th percentile acceleration for each player
@@ -267,3 +277,4 @@ plt.tight_layout()
 plt.savefig(base_dir / 'cross_plot.png', dpi=300, bbox_inches='tight')
 plt.show()
 print("Cross plot saved as 'cross_plot.png'")
+
